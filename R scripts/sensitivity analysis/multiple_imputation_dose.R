dec20e7686419694ce9275a4122171aca72b97ff
Bruce Zhou <huiyuan.zhou@duke.edu>
2024-11-30 23:38:11 -0500

    nov edits for the manuscripts

---
+++ R scripts/sensitivity analysis/multiple_imputation_dose.R
@@ -0,0 +1,63 @@
+# Multiple Imputation with Chained Equations with Re-parametrized Hearing Data
+# Author: Bruce Zhou
+# Date: `r Sys.Date()`
+
+# Load required libraries
+library(haven)
+library(dplyr)
+library(tidyverse)
+library(mice)
+library(ggmice)
+library(miceadds)
+library(micemd)
+
+# Clear workspace
+rm(list = ls())
+
+# Load data
+setwd("/Users/bruce/Desktop/GHRC Yan Lab/Hearing impairment & PF 2023")
+load('outputs/pooled_11_15_full.RData')
+
+pooled_11_15 <- pooled_11_15 %>%
+  dplyr::select(-c(gaitScore, standScore, SPPB))
+
+# Adjust column sequence
+var_list <- c("id", "visit", "hibpe", "diabe", "cancer", "lunge",
+              "hearte", "stroke", "arthre", "dyslipe", "livere", 
+              "kidneye", "asthmae", "marital", "smoke", "drink", 
+              "hr", "hhconsumpLevel", "mbmi", "education", "gait",
+              "cstand", "balance", "sex", "residence", "wave", "age")
+
+pooled_11_15_MI <- pooled_11_15 %>%
+  dplyr::select(all_of(var_list))
+
+pooled_11_15_MI <- pooled_11_15_MI %>%
+  mutate(across(all_of(c(3:20)), factor)) %>%
+  mutate(across(all_of(c(21:23)), as.numeric))
+
+# MICE setup
+ini <- mice(pooled_11_15_MI, maxit = 0)
+meth <- ini$meth
+
+# Specify imputation methods
+meth[3:16] <- "2l.bin" # Level-1 categorical variables
+meth[c("hr", "gait", "cstand", "hhconsumpLevel", "mbmi", "balance")] <- "2l.lmer" # Level-1 continuous/nominal variables
+meth[c("education")] <- "2lonly.pmm" # Level-2 variable
+
+# Setup predictor matrix
+imputeMatrix <- ini$predictorMatrix
+imputeMatrix[, "id"] <- -2 # Clustering variable
+imputeMatrix[imputeMatrix == 1] <- 2
+imputeMatrix[, "wave"] <- 0
+imputeMatrix[c("hhconsumpLevel", "mbmi", "balance"), ] <- 1
+imputeMatrix[c("hhconsumpLevel", "mbmi", "balance"), "id"] <- 0
+diag(imputeMatrix) <- 0
+
+# Imputation
+imp <- mice.par(pooled_11_15_MI, m = 30, maxit = 50, method = meth, 
+                predictorMatrix = imputeMatrix, seed = 2024)
+
+# Save the imputed dataset
+write.mice.imputation(mi.res = imp, name = "outputs/pooled_11_15_dose_imputed", mids2spss = FALSE)
+
+# End of script
--- README.md
+++ README.md
@@ -5,3 +5,30 @@ Repo for R codes with the project: Hearing Impairment and Physical Functioning
 Supervisor: **Prof. Lijing Yan**, **Zhengting He, ScM**.
 
 Project leader: **Bruce Zhou, BS**.
+
+# Instructions
+
+## Data Cleaning
+
+**charls_hearing_prep:** This cleans hearing + demographic covariates.
+**charls_physical_prep:** This cleans the SPPB and sub-items.
+**charls_covariates_prep:** This cleans the potential confounders.
+
+The order of these scripts does not matter as they are self-contained.
+
+## Data Merging
+
+**charls_stata:** This merges the 3 dataframes above to form the full\
+dataset. It exports to both R and Stata. Some of the first regressions\
+were done using Stata.
+
+## Multiple Imputation
+
+**multiple_imputation:** This implements a paralleled multiple imputation\
+with chained equations in R. The random seed has been set to 2024.
+
+## Statistical Analyses
+
+The statistical analyses scripts have been stored in each correspoding fol\
+der, namely `main analysis`, `secondary analysis`, `sensitivity analysis`,\
+and `subgroup analysis`.
